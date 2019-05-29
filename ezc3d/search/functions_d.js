400fdd644addad1d5b4c7e9f80c403ca78320b57
Pariterre <pariterre@hotmail.com>
2019-05-29 18:20:53 -0400

    Documentation for ezc3d 0.4.0

--- ezc3d/search/functions_d.js
+++ ezc3d/search/functions_d.js
@@ -1,12 +1,12 @@
 var searchData=
 [
-  ['read',['read',['../classezc3d_1_1ParametersNS_1_1GroupNS_1_1Group.html#adaa215458bcc9eb1d358cf1afc31e9b0',1,'ezc3d::ParametersNS::GroupNS::Group::read()'],['../classezc3d_1_1Header.html#a5261001c042b3ff72dfaf0d4eef2bd1a',1,'ezc3d::Header::read()'],['../classezc3d_1_1ParametersNS_1_1GroupNS_1_1Parameter.html#a8fe4bd0b960c299efbe52d55732bf5eb',1,'ezc3d::ParametersNS::GroupNS::Parameter::read()']]],
-  ['readfile',['readFile',['../classezc3d_1_1c3d.html#ac9bedefae9c5c48d0bc5532a23bac9de',1,'ezc3d::c3d']]],
-  ['readfloat',['readFloat',['../classezc3d_1_1c3d.html#a65f515720fe39d7f1350db7603ea47c0',1,'ezc3d::c3d']]],
-  ['readint',['readInt',['../classezc3d_1_1c3d.html#add76f668ac7e2bb470017cd808ec98e6',1,'ezc3d::c3d']]],
-  ['readparam',['readParam',['../classezc3d_1_1c3d.html#a5ae308f6a63f36bf12edff832520fa1c',1,'ezc3d::c3d::readParam(unsigned int dataLenghtInBytes, const std::vector&lt; size_t &gt; &amp;dimension, std::vector&lt; int &gt; &amp;param_data, size_t currentIdx=0)'],['../classezc3d_1_1c3d.html#af03306dd84fd315edc413ffd93c9bb02',1,'ezc3d::c3d::readParam(const std::vector&lt; size_t &gt; &amp;dimension, std::vector&lt; float &gt; &amp;param_data, size_t currentIdx=0)'],['../classezc3d_1_1c3d.html#a09282f6f61a6ef05f16ed4e088ef160d',1,'ezc3d::c3d::readParam(const std::vector&lt; size_t &gt; &amp;dimension, std::vector&lt; std::string &gt; &amp;param_data)']]],
-  ['readstring',['readString',['../classezc3d_1_1c3d.html#aa8d973a0fafe4449ce205a06d0fc8038',1,'ezc3d::c3d']]],
-  ['readuint',['readUint',['../classezc3d_1_1c3d.html#a07661231033b2ee19ed55b2ba44fdd05',1,'ezc3d::c3d']]],
+  ['read',['read',['../classezc3d_1_1ParametersNS_1_1GroupNS_1_1Group.html#a7b973eef7c3d543db746daf6d1f87fa6',1,'ezc3d::ParametersNS::GroupNS::Group::read()'],['../classezc3d_1_1Header.html#a4d91bccc2a98b3bebb2d7d46e19624f2',1,'ezc3d::Header::read()'],['../classezc3d_1_1ParametersNS_1_1GroupNS_1_1Parameter.html#afa029debc0d22a460500508d2676fed1',1,'ezc3d::ParametersNS::GroupNS::Parameter::read()']]],
+  ['readfile',['readFile',['../classezc3d_1_1c3d.html#a91d0df2779ed08a1dd5089d6ec4ac581',1,'ezc3d::c3d']]],
+  ['readfloat',['readFloat',['../classezc3d_1_1c3d.html#a142065bfe8f7204f5014d8f1207ccd25',1,'ezc3d::c3d']]],
+  ['readint',['readInt',['../classezc3d_1_1c3d.html#a44c1e83a720483558213b5df8745e061',1,'ezc3d::c3d']]],
+  ['readparam',['readParam',['../classezc3d_1_1c3d.html#ad8bedd3284f80f1a70ea266f56495481',1,'ezc3d::c3d::readParam(std::fstream &amp;file, unsigned int dataLenghtInBytes, const std::vector&lt; size_t &gt; &amp;dimension, std::vector&lt; int &gt; &amp;param_data, size_t currentIdx=0)'],['../classezc3d_1_1c3d.html#a21528990653cabba9aeffe93336253a3',1,'ezc3d::c3d::readParam(std::fstream &amp;file, const std::vector&lt; size_t &gt; &amp;dimension, std::vector&lt; float &gt; &amp;param_data, size_t currentIdx=0)'],['../classezc3d_1_1c3d.html#a4c3d9837aef8eb57533dfdfdb903b744',1,'ezc3d::c3d::readParam(std::fstream &amp;file, const std::vector&lt; size_t &gt; &amp;dimension, std::vector&lt; std::string &gt; &amp;param_data)']]],
+  ['readstring',['readString',['../classezc3d_1_1c3d.html#a6ee74f835690510d5f6ef556d0367990',1,'ezc3d::c3d']]],
+  ['readuint',['readUint',['../classezc3d_1_1c3d.html#a5aaf11338506cb16c70445cc0ad8f59e',1,'ezc3d::c3d']]],
   ['removetrailingspaces',['removeTrailingSpaces',['../namespaceezc3d.html#ac16a477c75a0c5f5e589da401a039aa9',1,'ezc3d']]],
   ['residual',['residual',['../classezc3d_1_1DataNS_1_1Points3dNS_1_1Point.html#acceaa873fd1dd55a12754b010138607f',1,'ezc3d::DataNS::Points3dNS::Point::residual() const'],['../classezc3d_1_1DataNS_1_1Points3dNS_1_1Point.html#aff1148c0abecb939de850f692e3fde33',1,'ezc3d::DataNS::Points3dNS::Point::residual(float residual)']]]
 ];
